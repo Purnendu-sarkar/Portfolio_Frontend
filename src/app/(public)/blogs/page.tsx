cd8862dbf4fe7cb22f0a159f77aff93292a4cf28
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-10-06 23:19:31 +0600

    fix: add image domain config for next/image and fetch blogs on All Blogs PAge

--- src/app/(public)/blogs/page.tsx
+++ src/app/(public)/blogs/page.tsx
@@ -1,9 +1,20 @@
-const AllBlogsPage = () => {
+import BlogCard from "@/components/modules/Blogs/BlogCard";
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export default async function AllBlogsPage() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/blogs`, {
+    cache: "no-store",
+  });
+  const result = await res.json();
+  const blogs = result?.data.blogs ?? [];
+  // console.log(blogs);
   return (
-    <div className="py-30 px-4 max-w-7xl mx-auto">
-      <h2 className="text-center text-4xl">All Blogs Page</h2>
+    <div className="container mx-auto px-4 py-28">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {blogs.map((blog: any) => (
+          <BlogCard key={blog?.id} data={blog} />
+        ))}
+      </div>
     </div>
   );
-};
-
-export default AllBlogsPage;
+}

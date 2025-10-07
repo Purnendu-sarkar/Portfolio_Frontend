9fbacc157babfb2b64bb78dd3355dda155cfe72b
Purnendu Sarkar <dsr102.purnendu@gmail.com>
2025-10-07 20:55:30 +0600

    feat(blogs): add ProjectsLoadingPage with loading spinner component

---
+++ src/app/(public)/projects/loading.tsx
@@ -0,0 +1,11 @@
+import Loading from "@/components/ui/Loading";
+
+const ProjectsLoadingPage = () => {
+  return (
+    <div className="py-28 px-4 max-w-7xl mx-auto flex justify-center items-center">
+      <Loading />
+    </div>
+  );
+};
+
+export default ProjectsLoadingPage;
